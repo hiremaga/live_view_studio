78ff9909faf1af1f245fcaa4cafa4c995a2a34d4
Abhi Hiremagalur <abhijit@hiremaga.com>
2021-02-24 15:26:06 -0800

    Sale countdown exercise

--- lib/my_app_web/live/license_live.ex
+++ lib/my_app_web/live/license_live.ex
@@ -5,7 +5,20 @@ defmodule MyAppWeb.LicenseLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
+
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
+    socket =
+      assign(socket,
+        seats: 2,
+        amount: Licenses.calculate(2),
+        expiration_time: expiration_time,
+        time_remaining: time_remaining(expiration_time)
+      )
+
     {:ok, socket}
   end
 
@@ -15,6 +28,13 @@ defmodule MyAppWeb.LicenseLive do
     <div id="license">
       <div class="card">
         <div class="content">
+          <p class="m-4 font-semibold text-indigo-800">
+          <%= if @time_remaining > 0 do %>
+            <%= format_time(@time_remaining) %> left to save 20%
+          <% else %>
+            Expired!
+          <% end %>
+          </p>
           <div class="seats">
             <img src="images/license.svg">
             <span>
@@ -39,5 +59,21 @@ defmodule MyAppWeb.LicenseLive do
     seats = String.to_integer(seats)
     socket = assign(socket, seats: seats, amount: Licenses.calculate(seats))
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
